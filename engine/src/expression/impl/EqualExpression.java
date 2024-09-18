254312077f70feb80dee13edcaaa421180c1011a
Rotem Zahavi <rotemzahavi99@gmail.com>
2024-09-18 13:08:35 +0300

    fixed the TRUE/FALSE strings to be upper case

--- engine/src/expression/impl/EqualExpression.java
+++ engine/src/expression/impl/EqualExpression.java
@@ -32,7 +32,8 @@ public class EqualExpression implements Expression {
             }
         }
 
-        return new EffectiveValueImpl(CellType.BOOLEAN, result);
+        String resultStr = result.toString().toUpperCase();
+        return new EffectiveValueImpl(CellType.BOOLEAN, resultStr);
     }
 
     @Override
