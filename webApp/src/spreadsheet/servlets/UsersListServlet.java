5724fff2b8f1ebdc7666c7a6da4e5866899ba8cb
Rotem Zahavi <rotemzahavi99@gmail.com>
2024-10-25 11:08:16 +0300

    fixed load file servlet and added users list servlet

---
+++ webApp/src/spreadsheet/servlets/UsersListServlet.java
@@ -0,0 +1,30 @@
+package spreadsheet.servlets;
+
+import com.google.gson.Gson;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import spreadsheet.utils.ServletUtils;
+import users.UserManager;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Set;
+
+@WebServlet("/userList")
+public class UsersListServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //returning JSON objects, not HTML
+        response.setContentType("application/json");
+        try (PrintWriter out = response.getWriter()) {
+            Gson gson = new Gson();
+            UserManager userManager = ServletUtils.getUserManager(getServletContext());
+            Set<String> usersList = userManager.getUsers();
+            String json = gson.toJson(usersList);
+            out.println(json);
+            out.flush();
+        }
+    }
+}
